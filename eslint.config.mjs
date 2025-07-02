2587400f4433ccb0752768e13e85bde32b760c3f
Rajat Dash <rohitdash16.rdx@gmail.com>
2025-07-02 18:53:35 +0530

    added husky eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+// eslint.config.mjs
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+const config = await tseslint.config(
+  { ignores: ['dist','node_modules', 'build'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+
+      // 👇 Add this line
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  }
+);
+
+export default config;
